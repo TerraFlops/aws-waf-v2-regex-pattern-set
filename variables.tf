8d77769c5a785ac8b08d815607c2754516f91562
jykingston <jy.kingston@gmail.com>
2020-10-07 08:30:06 +1100

    Add sane defaults

--- variables.tf
+++ variables.tf
@@ -11,11 +11,13 @@ variable "description" {
 variable "scope" {
   type = string
   description = "Specifies whether this is for an AWS CloudFront distribution or for a regional application. Valid values are CLOUDFRONT or REGIONAL. To work with CloudFront, you must also specify the region us-east-1 (N. Virginia) on the AWS provider."
+  default = "CLOUDFRONT"
 }
 
 variable "tags" {
   type = map(string)
   description = ""
+  default = {}
 }
 
 variable "regular_expressions" {
