df6af0977d50c04e022bcb2d86b7b205d20a7d3d
jykingston <jy.kingston@gmail.com>
2020-10-07 08:33:37 +1100

    Update output

--- output.tf
+++ output.tf
@@ -1,1 +1,13 @@
-# Insert output values here, if no outputs are defined delete this file
+output "arn" {
+  value = aws_wafv2_regex_pattern_set.regex_pattern_set.arn
+  description = "ARN of the Regex Pattern Set"
+}
+
+output "id" {
+  value = aws_wafv2_regex_pattern_set.regex_pattern_set.id
+  description = "ID of the Regex Pattern Set"
+}
+output "name" {
+  value = aws_wafv2_regex_pattern_set.regex_pattern_set.name
+  description = "Name of the Regex Pattern Set"
+}
